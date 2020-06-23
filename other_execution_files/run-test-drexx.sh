1f2517b236f0b2ae65dc58aa20fcd9a4e4f2cdd9
jonathanduque2 <jonasdg91@gmail.com>
2020-06-23 12:51:02 -0500

    [smart parameter assign v2.1] execution files for drexx problems

---
+++ other_execution_files/run-test-drexx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source exec_dre15.sh
+source exec_dre18.sh
+source exec_dre21.sh
+source exec_dre24.sh
+source exec_dre28.sh
+source exec_dre30.sh
+source exec_dre42.sh
+source exec_dre56.sh
+source exec_dre72.sh
+source exec_dre90.sh
+source exec_dre110.sh
+source exec_dre132.sh
