3908f6c174203d5e1101d42d816badf0dc1ec7ac
jonathanduque2 <jonasdg91@gmail.com>
2020-06-24 20:51:00 -0500

    [smart parameter assign v2.1] sh execution files for random probles BLM and CIM

---
+++ other_execution_files/run-test-randxx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source exec_rand36_bl.sh
+source exec_rand36_ci.sh
+source exec_rand49_bl.sh
+source exec_rand49_ci.sh
+source exec_rand64_bl.sh
+source exec_rand64_ci.sh
+source exec_rand81_bl.sh
+source exec_rand81_ci.sh
+source exec_rand100_bl.sh
+source exec_rand100_ci.sh
+source exec_rand121_bl.sh
+source exec_rand121_ci.sh
+source exec_rand144_bl.sh
+source exec_rand144_ci.sh
+
